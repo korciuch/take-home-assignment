7b3e9409699a42ebf2d2591521fe5d5836d07814
Kyle Orciuch <korciuch@stanford.edu>
2022-01-27 22:44:36 -0800

    Multiprocessing Added - v0.0.3

--- solve.py
+++ solve.py
@@ -1,68 +1,101 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 
 import sys
-from utils import Board, Coordinate, Shape, Lexicon
+import concurrent.futures
+from utils import Board, Coordinate, Shape, Lexicon, WorkUnit
+
+# List of all 8 possible neighbors in 2D space
 
 NEIGHBORS = [
-    Coordinate(-1,-1),
-    Coordinate(-1,0),
-    Coordinate(-1,1),
-    Coordinate(0,-1),
-    Coordinate(0,1),
-    Coordinate(1,-1),
-    Coordinate(1,0),
-    Coordinate(1,1),
+    Coordinate(-1, -1),
+    Coordinate(-1, 0),
+    Coordinate(-1, 1),
+    Coordinate(0, -1),
+    Coordinate(0, 1),
+    Coordinate(1, -1),
+    Coordinate(1, 0),
+    Coordinate(1, 1),
 ]
 
-def main(random_seed: int, shape: Shape) -> None :
-    results = []
+def main(random_seed: int, shape: Shape) -> None:
     # create random board
-    board = Board(random_seed=random_seed,shape=shape)
-    print(board.values)
+    board = Board(random_seed=random_seed, shape=shape)
+    print("BOARD: \n", board.values)
     # load in words for evaluation of the exploration algorithm
     lexicon = Lexicon('./words.txt')
-    for row in range(0,shape.height):
-        for column in  range(0,shape.width):
-            # define starting coordinate for depth first search
-            starting_coord = Coordinate(row,column)
-            # mark starting coord as visited
-            board.visited[starting_coord.r][starting_coord.c] = True
-            # define starting string
-            starting_str = board.values[starting_coord.r][starting_coord.c]
-            # solve board - get all valid words from current board
-            solve(board,starting_coord,starting_str,lexicon,results)
-            # done collecting words at this starting coord
-            # unmark as visited
-            board.visited[starting_coord.r][starting_coord.c] = False
-    print(results)
+    work_units = []
+    for row in range(0, shape.height):
+        for column in range(0, shape.width):
+            # define starting coordinate
+            starting_coord = Coordinate(row, column)
+            # define starting string to pass into solve
+            starting_char = board.values[starting_coord.r][starting_coord.c]
+            board.mark_as_visited(starting_coord)
+            unit = WorkUnit(starting_coord, starting_char, board, lexicon)
+            board.unmark_as_visited(starting_coord)
+            # append work units to list for multiprocessing
+            work_units.append(unit)
+    results = execute_work_units(work_units, lexicon)
+    print("RESULTS: ", results)
     return results
 
+
+# Multiprocessing: for each grid cell on board, call solve with corresponding
+# context. Since the execution times of the depth-first search algorithm vary,
+# we benefit from parallel over sequential execution.
+
+def execute_work_units(work_units: list, lexicon: Lexicon) -> set:
+    results = set()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+        future_to_unit = {
+            executor.submit(solve, unit.board, unit.coord, unit.char, lexicon, unit.data):
+            unit for unit in work_units
+        }
+        for future in concurrent.futures.as_completed(future_to_unit):
+            result = future.result()
+            if len(result) > 0:
+                results.update(set(result))
+    return results
+
+
+# Depth first search: starting at an arbitrary point on the board, find
+# all valid word combinations that originate from the starting coordinate.
+
 def solve(board: Board, curr_coord: Coordinate, curr_str: str, lexicon: Lexicon, positives: list) -> None:
-    # FAILURE: no prefix exists, prune current search path
+    # Failure: no prefix exists, prune current search path
     if not lexicon.is_prefix(curr_str):
         return
-    # SUCCESS: exact match, add to results
-    elif curr_str in lexicon.words:
+    # Success: exact string match, add to positive matches
+    elif curr_str in lexicon.words and curr_str not in positives:
         positives.append(curr_str)
-    # EXPLORE: expand search path to valid adjacent neighbors
+    # Explore: expand search path to valid adjacent neighbors
     for n in NEIGHBORS:
         new_r = curr_coord.r + n.r
         new_c = curr_coord.c + n.c
-        new_coord = Coordinate(new_r,new_c)
+        new_coord = Coordinate(new_r, new_c)
+        # don't go out of bounds or in already visited cell
         if board.is_within_bounds(new_coord) and not board.is_visited(new_coord):
             board.mark_as_visited(new_coord)
-            # if new coordinate is within bounds and hasn't been visited, append to search path
             curr_str += board.values[new_coord.r][new_coord.c]
-            # push new stack frame
-            solve(board,new_coord,curr_str,lexicon,positives)
-            # backtrack
+            # push new recursive stack frame
+            solve(board, new_coord, curr_str, lexicon, positives)
+            # backtrack, options exhausted
             board.unmark_as_visited(new_coord)
             curr_str = curr_str[:-1]
+
+    return positives
+
+
+# Main entrypoint for solve.py
+
 if __name__ == '__main__':
     if len(sys.argv) == 4:
         main(
             random_seed=int(sys.argv[1]),
-            shape=Shape(int(sys.argv[2]),int(sys.argv[3]))
+            shape=Shape(
+                int(sys.argv[2]),
+                int(sys.argv[3])
+            )
         )
     else:
         print("Expected usage: python3 solve.py <random_seed> <num_rows> <num_columns>")
--- unit_tests.py
+++ unit_tests.py
@@ -1,13 +1,24 @@
-import sys
-from tests import test1, test2, test3
+from tests import test1, test2, test3, test4
 
-TESTS = [test1, test2, test3]
+# List of tests to be evaluated
 
+TESTS = [test1, test2, test3, test4]
+
+# Checks correctness of basic test cases
 def run_unit_tests():
     correct = 0
     for t in TESTS:
-        correct += t()
-    print("PASSED {}/{} TESTS".format(correct,len(TESTS)))
+        result = t()
+        correct += result
+        if not result:
+            print("\nFAILED: ", t, '\n')
+        else:
+            print("\nPASSED: ", t, '\n')
+
+    print("PASSED {}/{} TESTS".format(correct, len(TESTS)))
+
+
+# Entrypoint for unit_tests.py
 
 if __name__ == '__main__':
     run_unit_tests()
